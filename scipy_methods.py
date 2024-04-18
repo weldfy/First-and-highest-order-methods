ed86afca189c6f4fe6bfb11bf1d6725afa676a17
weldfy <114976687+weldfy@users.noreply.github.com>
2024-04-18 22:54:11 +0300

    Update scipy_methods.py

--- scipy_methods.py
+++ scipy_methods.py
@@ -4,9 +4,9 @@ import numpy as np
 
 def newton_cg(f, df, hess, x0):
     return [np.ndarray.tolist(x) for x in reversed(
-            minimize(f, x0, method='Newton-CG', jac=df, options={'maxiter': 1000, 'return_all': True}, hess=hess).allvecs)]
+            minimize(f, x0, method='Newton-CG', jac=df, options={'maxiter': 1000, 'return_all': True}).allvecs)]
 
 
-def bfgs(f, df, hess, x0):
+def bfgs(f, df, hess, x0, c1=0.0001, c2=0.9):
     return [np.ndarray.tolist(x) for x in (
-            minimize(f, x0, method='BFGS', jac=df, options={'maxiter': 1000, 'return_all': True}, hess=hess).allvecs)]
+            minimize(f, x0, method='BFGS', jac=df, options={'maxiter': 1000, 'return_all': True, 'c1': c1, 'c2': c2}).allvecs)]
