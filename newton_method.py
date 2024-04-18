328f9adf8380137e6a97634d568cb534bb898aa9
weldfy <114976687+weldfy@users.noreply.github.com>
2024-04-18 20:33:57 +0300

    Update newton_method.py

--- newton_method.py
+++ newton_method.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 
-def newton_method(func, grad_func, hess_func, x, tol=1e-6, max_iter=1000):
+def newton_method(func, grad_func, hess_func, x, h=1, tol=1e-6, max_iter=1000):
     res = [x]
     for _ in range(max_iter):
         grad = grad_func(x)
         hess = hess_func(x)
         step = np.linalg.solve(hess, grad)
-        x = x - step
+        x = x - step * h
         res.append(np.ndarray.tolist(x))
         if np.linalg.norm(step) < tol:
             break
